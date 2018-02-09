0451231c2242e79cb2f3cc67a24859be20378f3b
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-09 11:26:58 +0200

    Add Header. Mod_01

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.header-slider').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   autoplay: false,
+   //autoplaySpeed: 5000,
+   dots: true,
+   dotsClass: 'slick-dots__header-slider',
+  });
+});

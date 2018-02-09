855be19382a1705ec113298d64f5a36f83bbcec2
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-09 15:01:54 +0200

    Add Section Tours

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   $('.header-slider').slick({
    slidesToShow: 1,
    slidesToScroll: 1,
-   autoplay: false,
-   //autoplaySpeed: 5000,
+   arrows: false,
+   autoplay: true,
+   autoplaySpeed: 5000,
    dots: true,
    dotsClass: 'slick-dots__header-slider',
   });
